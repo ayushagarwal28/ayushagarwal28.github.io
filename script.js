0174dd88a306b2ed44c022c11ddbcd9963061bb9
Ayush Agarwal <simplyayush28@gmail.com>
2021-08-07 22:20:03 +0530

    Added icon for dark mode

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (
     function() {
         var container = document.getElementById('container');
+        var [icon] = document.getElementsByTagName('i');
         var [h1] = document.getElementsByTagName('h1');
         var h2 = document.getElementsByTagName('h2');
         var paragraphs = document.getElementsByTagName('p');
@@ -9,6 +10,9 @@
         function setLightLayout() {
             container.classList.add('white-background')
             h1.classList.add('black');
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+            icon.style.color = 'white';
             for(var i = 0; i < h2.length; i++)
                 h2[i].classList.add('black');
             for(var i = 0; i < paragraphs.length; i++)
@@ -19,6 +23,9 @@
         function removeLightLayout() {
             container.classList.remove('white-background')
             h1.classList.remove('black');
+            icon.classList.add('fa-moon');
+            icon.classList.remove('fa-sun');
+            icon.style.color = 'black';
             for(var i = 0; i < h2.length; i++)
                 h2[i].classList.remove('black');
             for(var i = 0; i < paragraphs.length; i++)
@@ -34,7 +41,7 @@
         }
 
 
-        h1.addEventListener('click', function() {
+        icon.addEventListener('click', function() {
           inverseViewMode();
         });
     }
